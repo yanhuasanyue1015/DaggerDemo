8c8c929db426081ed70374ffbdb517b721060fb5
zhanglei <yanhuasanyue1015@gmail.com>
2016-09-28 02:10:01 +0800

    开始根据这个做一些操作: https://guides.codepath.com/android/Dependency-Injection-with-Dagger-2#overview

--- app/src/main/java/richmj/com/daggerdemo/AppModule.java
+++ app/src/main/java/richmj/com/daggerdemo/AppModule.java
@@ -1,8 +1,26 @@
 package richmj.com.daggerdemo;
 
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Created by zhanglei-account on 2016/9/28.
  */
-
+@Module
 public class AppModule {
+    private Application application;
+
+    public AppModule(Application application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    Application providesApplication() {
+        return application;
+    }
 }
